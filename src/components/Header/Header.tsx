62c2d60360edd28cd11666e9e9e7de2dd52cce44
ArdenJ <j.gaddas@outlook.com>
2020-01-09 22:36:02 +0000

    set up theming and global styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledHeader } from './Header.styled'
+
+export default function Header(): JSX.Element {
+  return (
+    <StyledHeader>
+      <h1>Hello I&apos;m Arden</h1>
+    </StyledHeader>
+  )
+}
