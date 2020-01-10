60c4020bd9148dba3d0584adee2f18a7ec7de817
ArdenJ <j.gaddas@outlook.com>
2020-01-10 13:50:19 +0000

    updating theme and package json

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import logo from './logo.svg'
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from './globalStyles/theme'
+import { GlobalStyles } from './globalStyles/global'
+
+import Header from './components/Header/Header'
+import Links from './components/Links/Links'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div className="App">
+        <Header>
+          <h1>Hello I&apos;m Arden</h1>
+          <h2>and I write hacky code</h2>
+          <Links />
+        </Header>
+      </div>
+    </ThemeProvider>
   )
 }
 
