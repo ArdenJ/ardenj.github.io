307586175d7ee00207de4d0dc00429198a12a948
ArdenJ <j.gaddas@outlook.com>
2020-01-10 08:29:22 +0000

    started developing component structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyledHeader } from './Header.styled'
 
-export default function Header(): JSX.Element {
+export default function Header(props: any): JSX.Element {
   return (
     <StyledHeader>
-      <h1>Hello I&apos;m Arden</h1>
+      <h1>{props.children}</h1>
     </StyledHeader>
   )
 }
---
+++ src/components/Links/Links.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Links(): JSX.Element {
+  return <div>these be links</div>
+}
