898014949e2d2dafa23734ab982803b2ba24cf87
ArdenJ <j.gaddas@outlook.com>
2020-01-27 23:12:10 +0000

    Updates

---
+++ precache-manifest.e06daf8b300d024d92820a5c9f0a8113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89b4e3f2e7380ac919fe62784f1219b",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "27994d9c66f850b88044",
+    "url": "/site/static/js/2.af63b108.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/site/static/js/2.af63b108.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0badb71a39d926e01db",
+    "url": "/site/static/js/main.65bf15ff.chunk.js"
+  },
+  {
+    "revision": "d1cc7ccfd6cade5e12d7",
+    "url": "/site/static/js/runtime-main.e972067c.js"
+  }
+]);
